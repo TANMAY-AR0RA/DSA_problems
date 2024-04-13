646038633af703bbf7ff55fa0a36f53511e4960e
Tanmay Arora <aroratanmay34@gmail.com>
2024-04-13 19:37:52 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4,5,6};
+        int i;
+        for(i=0;i<6;i++){
+            if(A[i]==5) System.out.print(i);
+        }
+    }
+}
